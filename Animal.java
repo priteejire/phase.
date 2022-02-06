d9c5a8858c71f8631350c3af6ee8ac554c9b1e0e
priteejire <98096887+priteejire@users.noreply.github.com>
2022-02-06 14:22:39 +0530

    Class

---
+++ Animal.java
@@ -0,0 +1,43 @@
+
+public class Animal {
+	
+	    String name; 
+	    String breed; 
+	    int age; 
+	    String color; 
+	    public Animal(String name, String breed, int age, String color) 
+	    { 
+	        this.name = name; 
+	        this.breed = breed; 
+	        this.age = age; 
+	        this.color = color; 
+	    } 
+	    public String getName() 
+	    { 
+	        return name; 
+	    } 
+	    public String getBreed() 
+	    { 
+	        return breed; 
+	    } 
+	    public int getAge() 
+	    { 
+	        return age; 
+	    } 
+	    public String getColor() 
+	    { 
+	        return color; 
+	    } 
+	    @Override
+	    public String toString() 
+	    { 
+	        return("Hi my name is "+ this.getName()+ ".\nMy breed,age and color are " + this.getBreed()+", " + this.getAge()+ ", and"+ this.getColor() + "."); 
+	    } 
+	    public static void main(String[] args) 
+	    { 
+	        Animal scott = new Animal("Tuffy","wild",6,"white"); 
+	        System.out.println(scott.toString()); 
+	    } 
+	}
+
+
