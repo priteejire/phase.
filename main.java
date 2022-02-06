d3beb3d368cb27e40a9014d7a42683c8de72c9b6
priteejire <98096887+priteejire@users.noreply.github.com>
2022-02-06 14:42:46 +0530

    Arrays

---
+++ main.java
@@ -0,0 +1,16 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		demo r = new demo();
+		int arr[] = { 21, 12, 13, 54, 15, 66, 57 }; 
+		r.rotate(arr, 5); 
+		for(int i=0;i<arr.length;i++){
+    			System.out.print(arr[i]+" ");
+		}
+}
+
+	}
+
+
